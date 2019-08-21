1adcfd88fd4a93b0d02e93aba5fbdb262e0028e9
ryascl <rys@pobox.com>
2019-08-21 16:30:14 -0700

    query params

--- WordSimilarity/main.py
+++ WordSimilarity/main.py
@@ -13,9 +13,21 @@ def pairings(source):
   return result
 
 def handler(event, context):
-    words = ['car', 'jet', 'dog', 'wheel', 'pizza', 'airport', 'city', 'friend']
+    queryString = event["queryStringParameters"]
+    words = queryString["words"]
+    if(words.len() < 2 or words.len() > 40):
+        return {
+            "statusCode": 400,
+            "status": "invalid request",
+            "body": json.dumps({
+                "message": "query parameter 'words' must have between 2 and 40 values"
+            })
+        }
+
     synsets = [{ "word": w, "lemma": wn.synsets(w, 'n')[0] } for w in words]
     pairs = pairings(synsets)
     similarities = [[p[0]["word"], p[1]["word"], p[0]["lemma"].wup_similarity(p[1]["lemma"])] for p in pairs]
 
-    return list(similarities)
+    return {
+        "body": json.dumps(list(similarities))
+    }
