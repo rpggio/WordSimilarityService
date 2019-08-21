fdf0abd8aac08ca3aa6343407fe7a37ce1975eed
ryascl <rys@pobox.com>
2019-08-21 15:12:51 -0700

    Downloading corpus to ephermal storage

--- WordSimilarity/main.py
+++ WordSimilarity/main.py
@@ -1,6 +1,13 @@
+import nltk
+import json
+from nltk.corpus import wordnet as wn
+
+nltk.data.path.append("/tmp")
+nltk.download("wordnet", download_dir = "/tmp")
+
 def handler(event, context):
     
     print(event)
     print(context)
-    
-    return {"message": "Successfully executed"}
+
+    return {"message": wn.synsets('cat', 'n')[0].name()}
