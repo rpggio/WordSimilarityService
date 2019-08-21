266ccee03f200f74ac9bfb0846983f67165e346a
ryascl <rys@pobox.com>
2019-08-21 16:08:11 -0700

    timeout

--- WordSimilarity/main.py
+++ WordSimilarity/main.py
@@ -5,9 +5,17 @@ from nltk.corpus import wordnet as wn
 nltk.data.path.append("/tmp")
 nltk.download("wordnet", download_dir = "/tmp")
 
-def handler(event, context):
-    
-    print(event)
-    print(context)
+def pairings(source):
+  result = []
+  for p1 in range(len(source)):
+          for p2 in range(p1+1, len(source)):
+                  result.append([source[p1],source[p2]])
+  return result
 
-    return {"message": wn.synsets('cat', 'n')[0].name()}
+def handler(event, context):
+    words = ['car', 'jet', 'dog', 'wheel', 'pizza', 'airport', 'city', 'friend']
+    synsets = [{ "word": w, "lemma": wn.synsets(w, 'n')[0] } for w in words]
+    pairs = pairings(synsets)
+    similarities = [[p[0]["word"], p[1]["word"], p[0]["lemma"].wup_similarity(p[1]["lemma"])] for p in pairs]
+
+    return list(similarities)
