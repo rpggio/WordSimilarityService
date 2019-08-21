286f2302245e5e7c1f40e4bcb560d2863b9df4b9
Ryan Smith <ryan.smith@pobox.com>
2019-08-21 15:28:02 -0700

    requirements testing

--- WordSimilarity/main.py
+++ WordSimilarity/main.py
@@ -1,13 +1,8 @@
 import nltk
-import json
-from nltk.corpus import wordnet as wn
-
-nltk.data.path.append("/tmp")
-nltk.download("wordnet", download_dir = "/tmp")
 
 def handler(event, context):
-    
+	
     print(event)
     print(context)
-
-    return {"message": wn.synsets('cat', 'n')[0].name()}
+	
+    return {"message": "Successfully executed"}
