9b478f1c9a4a78652a7fb5b273eb1d9f4212a9e4
ryascl <rys@pobox.com>
2019-08-22 18:03:04 -0700

    Cortical API similarities

---
+++ WordSimilarity/cortical-similarities.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+def pairings(source):
+  result = []
+  for p1 in range(len(source)):
+          for p2 in range(p1+1, len(source)):
+                  result.append([source[p1],source[p2]])
+  return result
+
+def parseWordsFromRequest(event):
+    if not "queryStringParameters" in event:
+        return []
+    queryString = event["queryStringParameters"]
+    if queryString is None or not "words" in queryString:
+        return []
+    return [w.strip() for w in queryString["words"].split(',')]
+
+def handler(event, context):
+    words = parseWordsFromRequest(event)
+    if(len(words) < 2 or len(words) > 30):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "query parameter 'words' must have between 2 and 40 values"
+            })
+        }
+
+response = requests.request(
+    method = "POST",
+    url = "http://api.cortical.io/rest/compare/bulk",
+    params = {
+      "retina_name": "en_associative"
+    },
+    data = json.dumps(data),
+    headers = {
+      "content-type": "application/json"
+      # "api-key": "c992aea0-c472-11e9-8f72-af685da1b20e"
+    }
+    )
+
+responseRows = json.loads(response.text)
+jaccards = [r["jaccardDistance"] for r in responseRows]
+distances = list(zip(pairs, jaccards))
+distances.sort(key=lambda dist: dist[1])
+
+    return {
+        "body": json.dumps(distances)
+    }
